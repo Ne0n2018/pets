e8a7bc9b3869a708681b3dcf3652d1b8ef7858d6
SergeyBaturo <86744691+SergeyBaturo@users.noreply.github.com>
2022-06-05 12:01:24 +0300

    Add files via upload
    All pages are done

--- firstPage.js
+++ firstPage.js
@@ -6,3 +6,10 @@ function changeOpacity() {
 }
 
 button.addEventListener('click', () => changeOpacity(textArea));
+
+const list = document.querySelectorAll('.list');
+        function activeLink() {
+            list.forEach((item) => item.classList.remove('active'));
+            this.classList.add('active');
+        }
+        list.forEach((item) => item.addEventListener('click', activeLink));
