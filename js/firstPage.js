60283a3fe4f1265d55a9a0c71d50dd1beb633232
kirill854 <104451280+kirill854@users.noreply.github.com>
2022-06-11 18:31:23 +0300

    Delete js directory

--- js/firstPage.js
+++
@@ -1,8 +0,0 @@
-const textArea = document.querySelector('div.choosePicture');
-const button = document.querySelector('div.addPhoto');
-
-function changeOpacity() {
-    document.querySelector('div.choosePicture').style.opacity = '1';
-}
-
-button.addEventListener('click', () => changeOpacity(textArea));
